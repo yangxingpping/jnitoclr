9376a5671aaf5ae082525235676a3aeb562d5b9a
alqaz <alqaz@94a652ec-0c30-4ba2-95e2-b2fb480755bb>
2015-03-03 01:47:57 +0000

    文件->函数->语句

---
+++ PythonApplication6/PythonApplication6/in.cpp
@@ -0,0 +1,24 @@
+JNIEXPORT jobject JNICALL
+Java_com_yuandian_cc
+(JNIEnv *env, jobject, jstring queryid_jstr){
+	//one line commit with //
+	return nullptr;
+	/*multiline commit in one line*/
+	/*multiline commit in 
+	multiline 
+	*/
+}
+
+JNIEXPORT jobject JNICALL
+Java_com_yuandian_second
+(JNIEnv *env, jobject, jstring queryid_jstr){
+	//one line commit with //
+	
+	return nullptr;
+	
+	/*multiline commit in one line*/
+	
+	/*multiline commit in 
+	multiline 
+	*/
+}
